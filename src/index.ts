4a0b54284160a6bc0be95f76a3047e5dd5d75244
sxpjb <emmanuel.hernandezramirez@gmail.com>
2024-03-03 14:21:01 -0600

    [SXPJB] test pre-commit

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,23 @@
 import app from './app';
-import {PORT} from './constants/constants';
-import {DatabaseConfig} from './config/db_conextion';
+import { PORT } from './constants/constants';
+import { DatabaseConfig } from './config/db_conextion';
 
 /**
  * @description Main function to initialize the server
  * @returns {Promise<void>}
  * @author SXPJB
  * **/
-const main = async () => {
-  try {
-    await DatabaseConfig.initialize();
-    app.listen(PORT, () => console.log(`Server on PORT: ${PORT}`));
-  } catch (e) {
-    console.error(e);
+class RunServer {
+  public static async main(): Promise<void> {
+    try {
+      await DatabaseConfig.initialize();
+      app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+      });
+    } catch (error) {
+      console.error('Error starting server: ', error);
+    }
   }
-};
+}
 
-main();
+RunServer.main();
